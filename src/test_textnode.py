8d205f0653506daf39d07a94238651244b7edcd8
Taxzosc <martinfh9@gmail.com>
2026-06-09 20:51:02 +0200

    finished text_node_to_html_node function, made lots of tests, it all passed, then altered the function a bit after talking to boots and seeing the solution. all tests are good, no change

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -33,5 +33,44 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
      #copied from boots ai learning tool after asking about missing tests.
      #"a nice to have rather than a genuine gap", since the init is default to None
+
+    def test_textnode_to_htmlnode_function_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_textnode_to_htmlnode_func_bold(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+    
+    def test_textnode_to_htmlnode_func_italic(self):
+        node = TextNode("This is an italic text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic text node")
+    
+    def test_textnode_to_htmlnode_func_code(self):
+        node = TextNode("This is a code text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code text node")
+    
+    def test_textnode_to_htmlnode_func_link(self):
+        node = TextNode("This is a link to bootdev node", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link to bootdev node")
+        self.assertEqual(html_node.props, {"href":"https://www.boot.dev"})
+    
+    def test_textnode_to_htmlnode_func_image(self):
+        node = TextNode("This is a link to bootdev image node", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src":"https://www.boot.dev", "alt": "This is a link to bootdev image node"})
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -24,17 +24,18 @@ class TextNode():
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 def text_node_to_html_node(text_node: TextNode) -> LeafNode:
-    if text_node.text_type not in TextType:
-        raise Exception("This is not a valid Text Type")
-    if text_node.text_type.value == "text":
-        return LeafNode(None, f"{text_node.text}")
+    # if text_node.text_type not in TextType:  #changing this to match the solution. while this will work, it can cause errors.
+    #     raise Exception("This is not a valid Text Type") #its better and cleaner to just have it at the end, because if none of the if statements gets hit, it must be a wrong input.
+    if text_node.text_type.value == "text":  #all of the If statements here can be written as "if text_node.text_type == TextType.Type(TEXT,BOLD etc)"
+        return LeafNode(None, text_node.text)  #removing the f-strings as this can cause an error if text_node.url is none, also redundant and not needed
     if text_node.text_type.value == "bold":
-        return LeafNode("b", f"{text_node.text}")
+        return LeafNode("b", text_node.text)    #removing the f-strings as this can cause an error if text_node.url is none, also redundant and not needed
     if text_node.text_type.value == "italic":
-        return LeafNode("i", f"{text_node.text}")
+        return LeafNode("i", text_node.text)    #removing the f-strings as this can cause an error if text_node.url is none, also redundant and not needed
     if text_node.text_type.value == "code":
-        return LeafNode("code", f"{text_node.text}")
+        return LeafNode("code", text_node.text) #removing the f-strings as this can cause an error if text_node.url is none, also redundant and not needed
     if text_node.text_type.value == "link":
-        return LeafNode("a", f"{text_node.text}", {"href":f"{text_node.url}"})
+        return LeafNode("a", f"{text_node.text}", {"href":text_node.url})
     if text_node.text_type.value == "image":
-        return LeafNode("img", "", {"src":f"{text_node.url}", "alt": text_node.text})
+        return LeafNode("img", "", {"src":text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}") #copied from solution, see reasoning above.
