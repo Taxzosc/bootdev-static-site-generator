0db8d01cd97988bb4061b28ce243f69ec84ab95b
TaxzoSC <Martinfh9@gmail.com>
2026-06-11 11:42:57 +0200

    added split_nodes_delimiter.py

---
+++ split_nodes_delimiter.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(
+        old_nodes: list[TextNode],
+        delimiter: str,
+        text_type: TextType) -> list[TextNode]:
+    pass
