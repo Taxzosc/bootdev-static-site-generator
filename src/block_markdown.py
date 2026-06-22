3a10e6b00343e0e69be579986da4cfa1eb1a0b85
Taxzosc <martinfh9@gmail.com>
2026-06-22 22:41:04 +0200

    started on markdown_to_html_node, and changed check_valid functions to be named helper_check_valid, for clarity

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import HTMLNode
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -8,7 +9,7 @@ class BlockType(Enum):
     UNORDERED_LIST = "unordered_list" #spelling error "unDorned_list"
     ORDERED_LIST = "ordered_list"
 
-def check_valid_headings(text: str) -> bool: 
+def helper_check_valid_headings(text: str) -> bool: 
     block = text.strip()
     # split_block = block.split("\n") #this check is wrong. there should never be a multiline heading.
     # count = "#"
@@ -20,7 +21,7 @@ def check_valid_headings(text: str) -> bool:
     if block.startswith(("# ","## ","### ","#### ","##### ","##### ","###### ")):
         return True
 
-def check_valid_quote(text: str) -> bool:
+def helper_check_valid_quote(text: str) -> bool:
     block = text.strip()
     split_block = block.split("\n")
     for line in split_block:
@@ -28,7 +29,7 @@ def check_valid_quote(text: str) -> bool:
             return False
     return True
 
-def check_valid_unordered_list(text: str) -> bool:
+def helper_check_valid_unordered_list(text: str) -> bool:
     block = text.strip()
     split_block = block.split("\n")
     for line in split_block:
@@ -37,7 +38,7 @@ def check_valid_unordered_list(text: str) -> bool:
     return True
 
 
-def check_valid_ordered_list(text: str) -> bool:
+def helper_check_valid_ordered_list(text: str) -> bool:
     block = text.strip()
     split_block = block.split("\n")
     count = 1
@@ -58,14 +59,20 @@ def markdown_to_blocks(markdown):
     return block_list
 
 def block_to_block_type(block: str) -> BlockType:
-    if check_valid_headings(block):
+    if helper_check_valid_headings(block):
         return BlockType.HEADING
     if len(block) > 1 and block[0:4] == "```\n" and block[-3:] == "```":  #this does not check if its a multiline code, as in, has mutliple lines, adding a len check like in the solution. 
         return BlockType.CODE
-    if check_valid_quote(block):
+    if helper_check_valid_quote(block):
         return BlockType.QUOTE
-    if check_valid_unordered_list(block):
+    if helper_check_valid_unordered_list(block):
         return BlockType.UNORDERED_LIST
-    if check_valid_ordered_list(block):
+    if helper_check_valid_ordered_list(block):
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        pass
