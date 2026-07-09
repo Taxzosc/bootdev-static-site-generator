c77bcd72740900e7e3100326e2df7276bddc3c4e
TaxzoSC <Martinfh9@gmail.com>
2026-07-09 12:22:26 +0200

    tried running it, it failed, fixed the error, submitted, success, checked with boots and soution, changed md_destination to not always be index.html

--- src/generate_content.py
+++ src/generate_content.py
@@ -1,5 +1,6 @@
 import os
 from block_markdown import markdown_to_blocks, markdown_to_html_node
+from pathlib import Path
 
 def extract_title(markdown: str) -> str:
     blocks = markdown_to_blocks(markdown)
@@ -42,19 +43,20 @@ def generate_page(from_path: str | os.PathLike, template_path: str | os.PathLike
 
 
 def generate_pages_recursive(dir_path_content: str | os.PathLike, template_path: str | os.PathLike, dest_dir_path: str | os.PathLike):
-    if len(os.listdir(dir_path_content)) == 0:
+    if len(os.listdir(dir_path_content)) == 0: #redundant as if a loop is fed an empty list, it does not execute and the function returns none
         return
         # raise ValueError("directory empty")
-    
     for item in os.listdir(dir_path_content):
-        
         item_path = os.path.join(dir_path_content,item)
         destination_item_path = os.path.join(dest_dir_path, item)
         
-        if os.path.isfile(item) and item[:-3] == ".md":
-            md_destination = os.path.join(dest_dir_path, "index.html")
-            print(f"inside rcursive : using generating page using {item_path}, {template_path}, {md_destination}")
+        if os.path.isfile(item_path) and item[-3:] == ".md":
+            # md_destination = os.path.join(dest_dir_path, "index.html")  #solution uses Path(dest_path).with_suffix(".html") to avoid every file be named "index.html".
+            #aka it creates a path object of the path to the html file.
+            #what if there is a md file that is not supposed to be named index
+            md_destination = Path(destination_item_path).with_suffix(".html") #copied after seeing solution and chatting with boots. the above solution works for this project, assuming every md is index.
+            print(f"inside recursive : using generating page using {item_path}, {template_path}, {md_destination}")
             generate_page(item_path, template_path, md_destination)
-        if os.path.isdir(item):
+        if os.path.isdir(item_path):
             print(f"recursing down to {item_path}")
             generate_pages_recursive(item_path, template_path, destination_item_path)
