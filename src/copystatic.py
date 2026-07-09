3f8f165be4dbbe52da7990719f90bea2b90125e6
TaxzoSC <Martinfh9@gmail.com>
2026-07-09 09:55:04 +0200

    moved extract title and generate page functions to its own module

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-from block_markdown import markdown_to_blocks, markdown_to_html_node
 
 
 def copy_static(src_path: str | os.PathLike, destination: str | os.PathLike):
@@ -27,46 +26,3 @@ def copy_static(src_path: str | os.PathLike, destination: str | os.PathLike):
 # while the deletion of the destination happends in main, before ever calling copy_static.
 # both work, but having a deletion section in the function makes it fragile?(according to boots)
 #also, my naming is not that clear. item path. solution uses from path, dest path. makes it more clear. could also name destination path as generated content path maybe?
-
-
-def extract_title(markdown: str) -> str:
-    blocks = markdown_to_blocks(markdown)
-    for text in blocks:
-        if text.startswith("# "):
-            return text[2:].strip()
-    raise ValueError("markdown contains no h1 header")
-
-def generate_page(from_path: str | os.PathLike, template_path: str | os.PathLike, dest_path: str | os.PathLike): 
-    # what is from_path going to be? is it the directory or is it supposed to be the md file itself? "read the md file at frompath"
-    print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
-    md_file = from_path
-    markdown = ""
-    # if os.path.isdir(md_file): #this is not needed as from_path should be pointing directly at the markdown file itself.
-    #     content_files = os.listdir(md_file)
-    #     for file in content_files: #doing this because i assume content can contain other things than the md file
-    #         if file[-3:] == ".md":
-    #             md_file = os.path.join(md_file,file)
-    #             break
-    open_md = open(md_file)
-    markdown = open_md.read()
-    open_md.close()
-
-    html_node = markdown_to_html_node(markdown)
-    html = html_node.to_html()
-    title = extract_title(markdown)
-
-    open_template = open(template_path)
-    template = open_template.read() #copies template content to variable
-    open_template.close()
-    swapped_title_content = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
-
-    destination_dir_path = os.path.dirname(dest_path) #this snippet is copied from the solution after talking to boots to understand it.
-    if destination_dir_path != "":
-        os.makedirs(destination_dir_path, exist_ok=True)
-    
-    new_file_name = dest_path
-    new_file = open(new_file_name, mode='w') #creates and opens for writing
-    new_file.write(swapped_title_content)
-    new_file.close()
-
-
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import os
 from textnode import TextNode, TextType
-from copystatic import copy_static, generate_page
+from copystatic import copy_static
+from generate_content import generate_page
 
 static_path = "./static"
 destination_path = "./public"
 content_path = "./content"
 template_path = "./template.html"
+
 def main() -> None:
     copy_static(static_path, destination_path)
     # generate_page("./content","./template.html","./public") #changed it to reflect what i am learning from the solution.
